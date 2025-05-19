468b84ac641b9571a933f7d93d4503d48260008b
kissagnesa <sindzse88@gmail.com>
2025-05-19 12:19:16 +0200

    AgentController's CRUD is done

--- Controllers/AgentController.cs
+++ Controllers/AgentController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Backend.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq.Expressions;
 
 namespace Backend.Controllers
 {
@@ -7,5 +9,102 @@ namespace Backend.Controllers
     [ApiController]
     public class AgentController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                using (var cx = new CustomerserviceContext())
+                {
+                    var agents = cx.Agents.ToList();
+                    return Ok(agents);
+                }            
+            }
+            catch (Exception)
+            { 
+                return StatusCode(400);
+            }
+        }
+
+        [HttpGet("{ById}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                using (var cx = new CustomerserviceContext())
+                {
+                    var agent = cx.Agents.FirstOrDefault(x => x.AgentId == id);
+                    if (agent == null)
+                        return NotFound("Nincs ilyen azonosítójú kolléga!");
+                    return Ok(agent);
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(400);
+            }
+        }
+
+        [HttpPost("NewAgent")]
+        public IActionResult PostAgent(string name)
+        {
+            try
+            {
+                using (var cx = new CustomerserviceContext())
+                {
+                    var agent = new Agent();
+                    agent.Name = name;
+                    cx.Agents.Add(agent);
+                    cx.SaveChanges();
+                    return Ok("Új kolléga hozzáadása sikeres!");
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(401, "Nincs jogosultsága új kolléga felvételéhez!");
+            }
+        }
+
+        [HttpPut("UpdateAgent")]
+        public IActionResult PutAgent(int id, string name)
+        {
+            try
+            {
+                using (var cx = new CustomerserviceContext())
+                {
+                    var agent = cx.Agents.FirstOrDefault(x => x.AgentId == id);
+                    if (agent == null)
+                        return NotFound("Nincs ilyen azonosítójú kolléga!");
+                    agent.Name = name;
+                    cx.SaveChanges();
+                    return Ok("Kolléga módosítása sikeres!");
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(401, "Nincs jogosultsága kolléga módosításához!");
+            }
+        }
+
+        [HttpDelete("DeleteAgent")]
+        public IActionResult DeleteAgent(int id)
+        {
+            try
+            {
+                using (var cx = new CustomerserviceContext())
+                {
+                    var agent = cx.Agents.FirstOrDefault(x => x.AgentId == id);
+                    if (agent == null)
+                        return NotFound("Nincs ilyen azonosítójú kolléga!");
+                    cx.Agents.Remove(agent);
+                    cx.SaveChanges();
+                    return Ok("Kolléga törlése sikeres!");
+                }
+            }
+            catch (Exception)
+            {
+                return StatusCode(401, "Nincs jogosultsága kolléga törléséhez!");
+            }
+        }
     }
 }
