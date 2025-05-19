241f2b4b31221460018825f0eea76ea5cc6d296b
kissagnesa <sindzse88@gmail.com>
2025-05-19 11:42:26 +0200

    Controllers are created

---
+++ Controllers/CallController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CallController : ControllerBase
+    {
+    }
+}
